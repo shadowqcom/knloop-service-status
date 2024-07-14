738bb33b6e5a7df345b1398c7fea2f9081b9a97d
unclejee <swatxhim@outlook.com>
2024-07-15 01:42:40 +0800

    [Automated] Update Health Check Logs

--- health-check.sh
+++ health-check.sh
@@ -24,34 +24,47 @@ echo "开始执行 ${#KEYSARRAY[@]} 个检测任务:"
 
 mkdir -p logs
 
+# 创建一个数组来保存所有子shell的PID
+pids=()
+
+# 对于每一个URL，启动一个子shell来执行检测
 for (( index=0; index < ${#KEYSARRAY[@]}; index++))
 do
   key="${KEYSARRAY[index]}"
   url="${URLSARRAY[index]}"
-  echo "[$key] 正在检测中······"
-
-  for i in 1 2 3 4; 
-  do
-    response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
-    if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
-      result="success"
+  # 在子shell中执行检测
+  (
+    echo "[$key] 正在检测中······"
+    
+    for i in 1 2 3; 
+    do
+      response=$(curl --write-out '%{http_code}' --silent --output /dev/null $url)
+      if [ "$response" -eq 200 ] || [ "$response" -eq 202 ] || [ "$response" -eq 301 ] || [ "$response" -eq 302 ] || [ "$response" -eq 307 ]; then
+        result="success"
+        break
+      else
+        result="failed"
+      fi
+      sleep 5
+    done
+    
+    dateTime=$(date +'%Y-%m-%d %H:%M')
+    if [[ $commit == true ]]
+    then
+      echo $dateTime, $result >> "logs/${key}_report.log"
+      # 保留2000条数据
+      echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
     else
-      result="failed"
+      echo "$dateTime, $result"
     fi
-    if [ "$result" = "success" ]; then
-      break
-    fi
-    sleep 5
-  done
-  dateTime=$(date +'%Y-%m-%d %H:%M')
-  if [[ $commit == true ]]
-  then
-    echo $dateTime, $result >> "logs/${key}_report.log"
-    # 保留2000条数据
-    echo "$(tail -2000 logs/${key}_report.log)" > "logs/${key}_report.log"
-  else
-    echo "$dateTime, $result"
-  fi
+  ) &
+  pids+=($!)
+done
+
+# 等待所有子shell完成
+for pid in "${pids[@]}"
+do
+  wait $pid
 done
 
 if [[ $commit == true ]]
