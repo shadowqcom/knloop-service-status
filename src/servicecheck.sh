42b00e122b1a4559cb063638d3f0d9bbcf153f43
unclejee <swatxhim@outlook.com>
2024-07-17 04:22:40 +0800

    sh

--- src/servicecheck.sh
+++ src/servicecheck.sh
@@ -1,3 +1,6 @@
+# 直接从环境变量读取
+KEY=$WEBHOOK_KEY
+
 commit=true
 origin=$(git remote get-url origin)
 if [[ $origin == *statsig-io/statuspage* ]]
@@ -33,6 +36,9 @@ do
   key="${KEYSARRAY[index]}"
   url="${URLSARRAY[index]}"
   
+  # 定义一个数组来存储三次都失败的URLs
+  failedUrls=()
+
   # 在子shell中执行检测
   (
     echo "[$key] 正在检测中······"
@@ -50,6 +56,11 @@ do
     fi
     sleep 5
   done
+
+    # 添加失败的URL到数组
+    if [ "$result" = "failed" ]; then
+      failedUrls+=("$url") 
+    fi
     
     dateTime=$(date +'%Y-%m-%d %H:%M')
     if [[ $commit == true ]]
@@ -71,13 +82,40 @@ do
 done
 
 echo "**********************************************"
-echo "检测完成，开始提交.log文件"
+echo "检测完成，开始推送企业微信"
+
+# 创建一个空字符串来存储失败的URLs
+local failedUrlsMessage=""
+
+# 遍历failedUrls数组，将每个URL添加到消息中
+for url in "${failedUrls[@]}"; do
+  failedUrlsMessage+="\n- $url"
+done
+
+# 构建JSON消息体
+local jsonMessage='{
+  "msgtype": "markdown",
+  "markdown": {
+    "content": "#### 服务健康检查失败通知\n> 以下URL未通过健康检查：\n'"$failedUrlsMessage"'
+  }
+}'
+
+# 使用curl发送消息
+if [ -n "$failedUrlsMessage" ]; then
+  curl -X POST \
+       -H 'Content-Type: application/json' \
+       -d "$jsonMessage" \
+       'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=$KEY'
+fi
+
+echo "**********************************************"
+echo "开始提交.log文件到仓库"
 
 if [[ $commit == true ]]
 then
   # 提交到仓库
-  git config --global user.name 'unclejee'
-  git config --global user.email 'swatxhim@outlook.com'
+  git config --global user.name 'Github Actions'
+  git config --global user.email 'Actions@knloop.com'
   git add -A --force ./logs/
   git commit -am '[Automated] Update Service Check Logs'
   git push
