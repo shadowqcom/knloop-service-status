35d73a77991d706de2a639ce287cd1c23f49dfa1
unclejee <swatxhim@outlook.com>
2026-03-01 06:16:59 +0800

    cleanup

--- src/js/getyear.js
+++
@@ -1,9 +0,0 @@
-// 更新页脚年份。
-export async function getyear() {
-  try {
-    var currentYearElement = document.getElementById("currentYear");
-    currentYearElement.textContent = new Date().getFullYear();
-  } catch (error) {
-    console.error("在更新年份时发生错误:", error);
-  }
-}
